d7a65b77d6791f6e5e5a492d2ae77ea55c24e734
Ahmed-Hanafy-Hasan <ahmdhnfy656@gmail.com>
2024-12-30 17:11:54 +0200

    final

--- src/data/MenuData.ts
+++ src/data/MenuData.ts
@@ -39,7 +39,7 @@ const menu_data: MenuItem[] = [
         id: 5,
         has_dropdown: false,
         title: "Investments",
-        link: "/#global",
+        link: "/login",
     },
     {
         id: 6,
